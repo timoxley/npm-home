43d46b6da0fc69cfa0d7ce7d822d0112cbbc1257
Tim Oxley <secoif@gmail.com>
2014-02-24 11:18:02 +0800

    Just cancel if user clicks anywhere.

--- index.js
+++ index.js
@@ -24,18 +24,16 @@
 
   nextDots.dots = [3, 2, 1, 0]
   nextDots.current = 0
-  messageEl.innerHTML = '<div class="npm-home-message-inner">Going&nbsp;to <a href="'+link+'">'+link+'</a>&nbsp;in&nbsp;<span class="npm-home-dots">3&hellip;</span><a class="npm-home-cancel" title="Cancel Redirect" href="#">&#10006;</a></div></div>'
+  messageEl.innerHTML = '<div class="npm-home-message-inner">Going&nbsp;to <a class="npm-home-link" href="'+link+'">'+link+'</a>&nbsp;in&nbsp;<span class="npm-home-dots">3&hellip;</span><a class="npm-home-cancel" title="Cancel Redirect" href="#">&#10006;</a></div></div>'
   var dotsEl = messageEl.querySelector('.npm-home-dots')
 
-  var cancelled = false
-  var dotsInterval
   var dotsInterval = setInterval(function() {
     var dots = nextDots()
     dotsEl.innerHTML = dots + '&hellip;'
     if (dots !== 0) return
     messageEl.classList.remove('visible')
     clearInterval(dotsInterval)
-    if (!cancelled) window.location.href = link
+    if (!cancel.cancelled) window.location.href = link
   }, 1000)
 
   var wrap = document.body.querySelector('#wrap header')
@@ -46,9 +44,23 @@
 
   messageEl.querySelector('.npm-home-cancel').addEventListener('click', function(e) {
     e.preventDefault()
-    cancelled = true
+    e.stopPropagation()
+    cancel('Ok!', 700)
+  })
 
-    messageEl.firstElementChild.innerHTML = 'OK!'
+  document.body.addEventListener('click', function(e) {
+    var elLink = messageEl.querySelector('.npm-home-link')
+    if (e.srcElement === elLink) return
+    cancel('Ok!', 200)
+  })
+
+  function cancel(message, timeout) {
+    if (cancel.cancelled) return
+    cancel.cancelled = true
+    message = message || 'Ok!'
+    timeout = timeout || 700
+
+    messageEl.firstElementChild.innerHTML = message
 
     clearInterval(dotsInterval)
     setTimeout(function() {
@@ -56,6 +68,7 @@
       messageEl.addEventListener('transitioned', function() {
         wrap.removeChild(messageEl)
       })
-    }, 500)
-  })
+    }, timeout)
+  }
+
 })()
